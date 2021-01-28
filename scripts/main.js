7e5e84484d675a5080bd0faedc58d38de9310815
stepan881 <stepanstepanov180@gmail.com>
2021-01-28 17:38:49 +0300

    add scroll

--- scripts/main.js
+++ scripts/main.js
@@ -203,3 +203,19 @@ function renamePopup(name) {
 
 }
 
+const scroll = () => {
+  const smoothLinks = document.querySelectorAll('.js-scroll');
+  
+  for (let smoothLink of smoothLinks) {
+      smoothLink.addEventListener('click', function (e) {
+          e.preventDefault();
+          const id = smoothLink.getAttribute('href');
+     
+          document.querySelector(id).scrollIntoView({
+              behavior: 'smooth',
+              block: 'start'
+          });
+      });
+  };
+}
+scroll();
